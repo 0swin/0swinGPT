4506dd1c8bbd35e9d648c0f5a0b0a8880804f184
0swin <me@oswin.io>
2023-03-29 18:53:49 +0200

    Refactor messageCreate function

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,74 +1,74 @@
-const https = require('https');
-const Readability = require('@mozilla/readability');
-const { JSDOM } = require('jsdom');
+const https = require("https");
+const { Readability } = require("@mozilla/readability");
+const { JSDOM } = require("jsdom");
 
 async function handleMessageCreate(client, msg, openai) {
   if (msg.author.bot) return; // Ignore messages from bots
 
   // Check if the bot is mentioned
-  if (msg.mentions.has(client.user)) {
-    let userInput = msg.content.replace(/<@!?[0-9]+>/g, '').trim(); // Remove the mention from the message content
+  if (!msg.mentions.has(client.user)) return;
 
-    // Replace links in user input with their content
-    const linkRegex = /((?:https?|ftp):\/\/[\n\S]+)/g;
-    const links = userInput.match(linkRegex);
+  let userInput = msg.content.replace(/<@!?\d+>/, "").trim();
 
-    if (links) {
-      for (const link of links) {
-        const response = await new Promise((resolve, reject) => {
-          https.get(link, resolve).on('error', reject);
+  // Replace links in user input with their content
+  const linkRegex = /((?:https?|ftp):\/\/[\n\S]+)/g;
+  const links = userInput.match(linkRegex);
+
+  if (links) {
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      const response = await new Promise((resolve, reject) => {
+        https.get(link, resolve).on("error", reject);
+      });
+      const html = await new Promise((resolve) => {
+        let data = "";
+        response.on("data", (chunk) => {
+          data += chunk;
         });
-        const html = await new Promise(resolve => {
-          let data = '';
-          response.on('data', chunk => (data += chunk));
-          response.on('end', () => resolve(data));
+        response.on("end", () => {
+          resolve(data);
         });
-        const doc = new JSDOM(html, { url: link });
-        const reader = new Readability(doc.window.document);
-        const article = reader.parse();
-        userInput = userInput.replace(link, `${link} - ${article.textContent}`);
-      }
+      });
+      const doc = new JSDOM(html, { url: link });
+      const reader = new Readability(doc.window.document);
+      const article = reader.parse();
+      userInput = userInput.replace(link, `${link} - ${article.textContent}`);
     }
-
-    links.forEach((link, index) => {
-      userInput = userInput.replace(link, `${link} - ${pageContents[index]}`);
-    });
   }
 
   const thinkingMessage = await msg.reply("I'm thinking...");
 
-  // Fetch the last 6 messages in the channel
   const fetchedMessages = await msg.channel.messages.fetch({ limit: 6 });
   const conversation = fetchedMessages
-    .filter(m => m.id !== msg.id) // Exclude the current message
-    .map(m => `${m.author.username}: ${m.content}`)
+    .filter((m) => m.id !== msg.id) // Exclude the current message
+    .map((m) => `${m.author.username}: ${m.content}`)
     .reverse(); // Reverse the order to make it chronological
 
   const messageObjects = [
     {
-      role: 'system',
+      role: "system",
       content:
-        'You are a ChatGPT bot in a Discord conversation. If you need to mention someone, use <@user-id>',
+        "You are a ChatGPT bot in a Discord conversation. If you need to mention someone, use <@user-id>",
     },
-    ...conversation.map(line => {
-      const [author, content] = line.split(': ');
-      const role = author === client.user.username ? 'assistant' : 'user';
-      const mentionString = role === 'user' ? `<@${msg.author.id}>` : '';
+    ...conversation.map((line) => {
+      const [author, content] = line.split(": ");
+      const role = author === client.user.username ? "assistant" : "user";
+      const mentionString = role === "user" ? `<@${msg.author.id}>` : "";
 
       return {
         role,
         content: `${mentionString}${
-          role === 'user' ? ': ' : ''
+          role === "user" ? ": " : ""
         }${content.trim()}`,
       };
     }),
   ];
 
-  messageObjects.push({ role: 'user', content: userInput });
+  messageObjects.push({ role: "user", content: userInput });
   console.log(messageObjects);
 
   const completion = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    model: "gpt-3.5-turbo",
     max_tokens: 600,
     n: 1,
     stop: null,
@@ -78,12 +78,11 @@ async function handleMessageCreate(client, msg, openai) {
   console.log(completion.data.choices[0].message.content);
 
   const chatGPTResponse =
-    completion.data.choices[0].message.content.trim() || '';
+    completion.data.choices[0].message.content.trim() || "";
 
-  // Check if the response is empty or contains only whitespace
   if (!chatGPTResponse) {
     await thinkingMessage.edit(
-      "I'm sorry, I couldn't generate a response. Please try again.",
+      "I'm sorry, I couldn't generate a response. Please try again."
     );
   } else {
     await thinkingMessage.edit(chatGPTResponse);
